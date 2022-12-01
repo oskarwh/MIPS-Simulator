5afd0d18cc835c6d4b8e162a3bd1306d30189d13
Jakob Lindehag <c20jlg@cs.umu.se>
2022-12-01 19:38:24 +0100

    added receive method with input-id

--- mips_sim/src/units/instruction_memory.rs
+++ mips_sim/src/units/instruction_memory.rs
@@ -8,79 +8,94 @@ mod instruction_memory {
         current_instruction : BitArr::<LocalBits, usize>;
         current_address : u32;
         has_address : mut bool;
-        pc : &program_counter;
-        reg : &registers;
-        sign_extend : &sign_extend;
-        alu_ctrl : &alu_control;
+   
+        mux1 : &impl Mux;
+
+        reg : &impl Unit;
+        pc : &impl Unit;
+        sign_extend : &impl Unit;
+        alu_ctrl : &impl Unit;
+        control : &impl Unit;
+
+        //input id´s
+        read_address_id: u32; 
     }
 
 
     impl instruction_memory{
-        pub fn new(instr: Vec<u32>, pc: program_counter) -> instruction_memory{
+        //Define input nad output-id´s. TODO: Think about how this can be done nicer
+        const READ_ADDRESS = 1;
+        const MUX1_ID = 1;
+        const REG_READ_1_ID = 0;
+        const REG_READ_2_ID = 0;
+        const CTRL_IN_ID = 0;
+        //      .
+        //      .
+        //TODO: extend this
 
-            instruction_memory{instructions: instr, has_address:false}
+        pub fn new(
+            instr: Vec<u32>,
+            mux1: &impl Mux, 
+            reg: &impl Unit,
+            pc: &impl Unit,
+            sign_extend : &impl Unit,
+            alu_ctrl : &impl Unit,
+            ctrl : &impl Unit;
+
+        ) -> instruction_memory{
+            
+            instruction_memory{
+                instructions: instr, 
+                has_address:false,
+                mux1: mux1,
+                mux2: mux2,
+                reg:reg,
+                pc:pc,
+                sign_extend:sign_extend,
+                alu_ctrl:alu_ctrl,
+                control:ctrl,
+
+                read_address_id : READ_ADDRESS,
+            }
         }
 
         pub fn execute(&self){
             while(stop){
 
                 if(has_address){
-                    current_address= pc.get_address();
+                    //Received address on read_address! Find corresponding instruction
                     current_instruction = instructions[current_address >> 2];
-                    has_instruction = true;
+
+                    //Send instruction to other units
+                    mux1.receive(MUX1_ID, current_instruction[0...25]<<2 );//Todo: Think about how to concat with ADD output
+                    reg.receive(REG_READ_1_ID, current_instruction[21...25]);
+                    reg.receive(REG_READ_2_ID, current_instruction[16...20]);
+                    control.receive(CTRL_IN_ID, current_instruction[26...31]);
                 }
                 
             } 
         }
 
-        pub fn stop(){
+        pub fn stop(&self){
             stop =true;
-        }
-
-        fn get_instruction(index_a: usize, index_b : usize)-> BitSlice::<LocalBits, usize>;{
-            current_instruction[index_a...index_b]
         }
 
     }
 
- 
+    impl Unit for instruction_memory{
+
+
+        pub fn receive(&self, input_id: u32, address : BitArr::<LocalBits, usize>){
+            if input_id ==  self.read_address_id{
+                current_address = address;
+                has_address = true;
+            }else{
+                //Message came on undefined input
+            }
+            
+        }
+    }
 
 }
 
 
-/*  
-    impl instruction_memory{
-        pub fn new(instr: Vec<u32>, pc: program_counter) -> instruction_memory{
-
-            instruction_memory{instructions: instr, has_address:false}
-        }
-
-        pub fn execute(&self){
-            while(stop){
-
-                if(pc.has_address()){
-                    current_address= pc.get_address();
-                    current_instruction = instructions[current_address >> 2];
-                    has_instruction = true;
-                }
-                
-            } 
-        }
-
-        pub fn stop(){
-            stop =true;
-        }
-
-        fn get_instruction(index_a: usize, index_b : usize)-> BitSlice::<LocalBits, usize>;{
-            current_instruction[index_a...index_b]
-        }
-
-    }
-
-    impl instruction_memory for MuxInputer{
-        fn get_mux_input(&self) -> BitArr::<LocalBits, usize>{
-
-        }
-    }
-
-}*/
--- mips_sim/src/units/misc_units.rs
+++ mips_sim/src/units/misc_units.rs
@@ -1,26 +1,73 @@
+pub trait Unit{
+    pub fn receive(&self, input_id : u32, data : BitSlice::<LocalBits, usize>;);
+}
+
 mod mux{
-  
+    
     struct mux {
-        val1 : BitSlice::<LocalBits, usize>;
-        val2 : BitSlice::<LocalBits, usize>;
+        data0 : BitSlice::<LocalBits, usize>;
+        data1 : BitSlice::<LocalBits, usize>;
+
         signal : bool;
-        input1 : MuxInputer;
-        input2 : MuxInputer;
+        output_unit : &impl Unit;
+        output_id : u32;
+
+        input0_id : u32;
+        input1_id : u32;
+
+        has_val0 : bool;
+        has_val1 : bool;
     }
+
+    pub trait Mux{
+        pub fn new(instr: Vec<u32>, out: &impl Unit) -> self; 
+    }
+
     
 
-    impl mux{
+    impl Mux for mux{
+        //Define input-id´s
+        const IN0_ID = 0;
+        const IN1_ID = 1;
 
+        pub fn new(instr: Vec<u32>, out: &impl Unit, out_id : u32) -> mux{
+            
+            mux{
+                output_unit: out,
+                output_id: out_id,
+                signal : false,
+                output_unit : out,
+                input0_id : IN0_ID,
+                input1_id : IN1_ID,
+            }
+        }
 
+        fn execute(&self){
+            if signal{
+                output_unit.receive(self.output_id, data1);
+            }else{
+                output_unit.receive(self.output_id, data0);
+            }
+        }
+    }
+
+    impl Unit for mux{
+        pub fn receive(&self, input_id: u32, data : BitArr::<LocalBits, usize>){
+            if input_id = &self.input0_id {
+                data0 = data;
+            }else if input_id = &self.input1_id {
+                data1 = data;
+            }else{
+                //Data came on undefined input_id
+            }
+        }
     }
 
 
 }
 
 
-pub trait Unit{
-    fn receive()
-}
+
 
 
 
