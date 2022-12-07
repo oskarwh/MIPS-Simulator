5c7053fa80041a707f00428f74c94f77fa623f39
Oskar Westerlund Holmgren <c20own@cs.umu.se>
2022-12-07 23:23:42 +0100

    Have looked at PC and how we should use threads for each component

--- mips_sim/src/units/program_counter.rs
+++ mips_sim/src/units/program_counter.rs
@@ -4,35 +4,34 @@ mod program_counter {
 
     struct program_counter {
 
-        instructions : Vec<BitArr::<LocalBits, usize> >;
-        current_address : u32;
-        pc : program_counter;
+        current_address : BitVec::<LocalBits, usize>;
+        instr_mem: instruction_memory;
     }
 
 
-    impl program_counter{
+    impl Unit for program_counter{
         pub fn new() -> program_counter{
-
-            instruction_memory{instructions: instr, pc: pc}
+            //instruction_memory{instructions: instr, pc: pc}
+            current_address = bitvec![0;32];
         }
 
         pub fn execute(){
             while(){
-                
+                // Check when to send new instruction to instruction memory
+
+                // Send next address to instr memmory
+                instr_mem.receive_signal(self.IM_READ_ADDRESS_ID, current_address)
             }
-            
         }
 
-        pub fn get_instruction(index_a: usize, index_b : usize)-> BitSlice::<LocalBits, usize>;{
-            &instructions[current_address >> 2][index_a...index_b]
+        pub fn receive(&self, input_id : u32, data : BitVec::<LocalBits, usize>) {
+            // Set current address to the incoming adress.
+            current_address = data;
         }
 
-        fn read_address(){
-            current_address = pc.get_address();
+        pub fn set_instr_memory(&self, instr_mem: &Unit) {
+            self.instr_mem = instr_mem;
         }
-
-        
-
     }
 
 }
