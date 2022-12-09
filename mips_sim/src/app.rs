b53e0c42bdc16b6fcad1ea00b6f53a7b223cb55a
Max Thorén <c20mtn@cs.umu.se>
2022-12-09 10:58:08 +0100

    let's gooo

--- mips_sim/src/app.rs
+++ mips_sim/src/app.rs
@@ -1,9 +1,9 @@
-use tracing_subscriber::{registry::Data, fmt::writer};
 use eframe::egui;
 use egui::{FontFamily, FontId, RichText, TextStyle};
-use egui_extras::{StripBuilder, TableBuilder, Size};
+use egui_extras::{Size, StripBuilder, TableBuilder};
+use tracing_subscriber::{fmt::writer, registry::Data};
 #[derive(Debug, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))] 
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 enum DataFormat {
     Hex,
     Dec,
@@ -32,7 +32,7 @@ fn configure_text_styles(ctx: &egui::Context) {
         (heading3(), FontId::new(23.0, Proportional)),
         (Body, FontId::new(18.0, Proportional)),
         (Monospace, FontId::new(14.0, Proportional)),
-        (Button, FontId::new(14.0, Proportional)),
+        (Button, FontId::new(24.0, Proportional)),
         (Small, FontId::new(10.0, Proportional)),
     ]
     .into();
@@ -40,7 +40,7 @@ fn configure_text_styles(ctx: &egui::Context) {
 }
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 /// if we add new fields, give them default values when deserializing old state
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))] 
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct MipsApp {
     // Example stuff:
     selected: DataFormat,
@@ -63,15 +63,7 @@ impl Default for MipsApp {
 impl MipsApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        //if let Some(storage) = cc.storage {
-        //    return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        //}
-        configure_text_styles(&cc.egui_ctx);    
+        configure_text_styles(&cc.egui_ctx);
         Default::default()
     }
 
@@ -80,128 +72,156 @@ impl MipsApp {
             DataFormat::Dec => String::from(format!("{}", number)),
             DataFormat::Hex => String::from(format!("{:#010x}", number)),
             DataFormat::Bin => String::from(format!("{:#010b}", number)),
-        }    
-    } 
+        }
+    }
 
     fn memory_table(data_format: &DataFormat, ui: &mut egui::Ui) {
         ui.push_id(1, |ui| {
             TableBuilder::new(ui)
-            .striped(true)
-            .column(Size::remainder().at_least(200.0))
-            .column(Size::remainder().at_least(200.0))
-            .resizable(true)
-            .header(30.0, |mut header| {
-                header.col(|ui| {
-                    ui.label(RichText::new("Address").text_style(heading2()).strong());;
-                });
-                header.col(|ui| {
-                    ui.label(RichText::new("Data").text_style(heading2()).strong());;
-                });
-            })
-            .body(|mut body| {
-                for row_index in 0..30 {
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(row_index * 4,data_format));
+                .striped(true)
+                .column(Size::remainder().at_least(200.0))
+                .column(Size::remainder().at_least(200.0))
+                .resizable(true)
+                .header(30.0, |mut header| {
+                    header.col(|ui| {
+                        ui.label(RichText::new("Address").text_style(heading2()).strong());
+                    });
+                    header.col(|ui| {
+                        ui.label(RichText::new("Data").text_style(heading2()).strong());
+                    });
+                })
+                .body(|mut body| {
+                    for row_index in 0..30 {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(row_index * 4, data_format));
+                            });
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(0, data_format));
+                            });
                         });
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(0,data_format));
-                        });
-                    }); 
-                }
-            });
+                    }
+                });
         });
-        
     }
 
     fn instruction_table(data_format: &DataFormat, ui: &mut egui::Ui) {
-
+        ui.push_id(2, |ui| {
+            TableBuilder::new(ui)
+                .striped(true)
+                .column(Size::remainder().at_least(100.0))
+                .column(Size::remainder().at_least(100.0))
+                .column(Size::remainder().at_least(100.0))
+                .resizable(true)
+                .header(30.0, |mut header| {
+                    header.col(|ui| {
+                        ui.label(RichText::new("Address").text_style(heading2()).strong());
+                    });
+                    header.col(|ui| {
+                        ui.label(
+                            RichText::new("Machine code")
+                                .text_style(heading2())
+                                .strong(),
+                        );
+                    });
+                    header.col(|ui| {
+                        ui.label(RichText::new("Instruction").text_style(heading2()).strong());
+                    });
+                })
+                .body(|mut body| {
+                    for row_index in 0..500 {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.heading(MipsApp::write_int(row_index * 4, data_format));
+                            });
+                            row.col(|ui| {
+                                ui.heading(MipsApp::write_int(row_index * 3, data_format));
+                            });
+                            row.col(|ui| {
+                                ui.heading(MipsApp::write_int(0, data_format));
+                            });
+                        });
+                    }
+                });
+        });
     }
 
     fn register_table(data_format: &DataFormat, ui: &mut egui::Ui) {
         ui.push_id(3, |ui| {
             TableBuilder::new(ui)
-            .striped(true)
-            .column(Size::remainder().at_least(100.0))
-            .column(Size::remainder().at_least(100.0))
-            .column(Size::remainder().at_least(100.0))
-            .resizable(true)
-            .header(30.0, |mut header| {
-            header.col(|ui| {
-                ui.label(RichText::new("Register").text_style(heading2()).strong());;
-            });
-            header.col(|ui| {
-                ui.label(RichText::new("Name").text_style(heading2()).strong());;
-            });
-            header.col(|ui| {
-                ui.label(RichText::new("Data").text_style(heading2()).strong());;
-            });
-            })
-            .body(|mut body| {
-                for row_index in 0..500 {
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(row_index * 4,data_format));
+                .striped(true)
+                .column(Size::remainder().at_least(100.0))
+                .column(Size::remainder().at_least(100.0))
+                .column(Size::remainder().at_least(100.0))
+                .resizable(true)
+                .header(30.0, |mut header| {
+                    header.col(|ui| {
+                        ui.label(RichText::new("Register").text_style(heading2()).strong());
+                    });
+                    header.col(|ui| {
+                        ui.label(RichText::new("Name").text_style(heading2()).strong());
+                    });
+                    header.col(|ui| {
+                        ui.label(RichText::new("Data").text_style(heading2()).strong());
+                    });
+                })
+                .body(|mut body| {
+                    for row_index in 0..500 {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(row_index * 4, data_format));
+                            });
+                            row.col(|ui| {
+                                ui.label("$temp");
+                            });
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(0, data_format));
+                            });
                         });
-                        row.col(|ui| {
-                            ui.label("$temp");
-                        });
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(0,data_format));
-                        });
-                    }); 
-                }
-            }); 
-        });   
+                    }
+                });
+        });
     }
 
     fn symbol_table(data_format: &DataFormat, ui: &mut egui::Ui) {
         ui.push_id(4, |ui| {
             TableBuilder::new(ui)
-            .striped(true)
-            .column(Size::remainder().at_least(200.0))
-            .column(Size::remainder().at_least(200.0))
-            .resizable(true)
-            .header(30.0, |mut header| {
-                header.col(|ui| {
-                    ui.label(RichText::new("Address").text_style(heading2()).strong());;
-                });
-                header.col(|ui| {
-                    ui.label(RichText::new("Data").text_style(heading2()).strong());;
-                });
-            })
-            .body(|mut body| {
-                for row_index in 0..30 {
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(row_index * 4,data_format));
+                .striped(true)
+                .column(Size::remainder().at_least(200.0))
+                .column(Size::remainder().at_least(200.0))
+                .resizable(true)
+                .header(30.0, |mut header| {
+                    header.col(|ui| {
+                        ui.label(RichText::new("Address").text_style(heading2()).strong());
+                    });
+                    header.col(|ui| {
+                        ui.label(RichText::new("Data").text_style(heading2()).strong());
+                    });
+                })
+                .body(|mut body| {
+                    for row_index in 0..30 {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(row_index * 4, data_format));
+                            });
+                            row.col(|ui| {
+                                ui.label(MipsApp::write_int(0, data_format));
+                            });
                         });
-                        row.col(|ui| {
-                            ui.label(MipsApp::write_int(0,data_format));
-                        });
-                    }); 
-                }
-            });
-        }); 
-        
+                    }
+                });
+        });
     }
-
-    
 }
 
 impl eframe::App for MipsApp {
-    /// Called by the frame work to save state before shutdown.
-    //fn save(&mut self, storage: &mut dyn eframe::Storage) {
-    //    eframe::set_value(storage, eframe::APP_KEY, self);
-    //}
-
     /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            selected, 
+            selected,
             label,
-             value, } = self;
+            value,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -220,63 +240,86 @@ impl eframe::App for MipsApp {
             });
         });
 
-        egui::SidePanel::left("left_side_panel").resizable(false).show(ctx, |ui| {
-            ui.heading("Data memory panel");
-            ui.vertical(|ui| {
-                MipsApp::memory_table(&selected, ui);
-            });
-        });
-
-        // Create right panel 
-        egui::SidePanel::right("right_side_panel").resizable(false).show(ctx, |ui| {
-            
-            
-            // The right panel holding information about registers. 
-            ui.horizontal(|ui| {
-                ui.label("PC: 0");
-                egui::ComboBox::from_label("Data format")
-                .selected_text(format!("{:?}", selected))
-                .show_ui(ui, |ui| {
-                    // TODO: Add to so it actually impacts format of values.
-                    ui.selectable_value( selected, DataFormat::Hex, "Hex");
-                    ui.selectable_value( selected, DataFormat::Dec, "Decimal");
-                    ui.selectable_value( selected, DataFormat::Bin, "Binary");
+        egui::SidePanel::left("left_side_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.heading("Data memory panel");
+                ui.vertical(|ui| {
+                    MipsApp::memory_table(&selected, ui);
                 });
             });
 
-            // Create tables for registers and symbols
-            StripBuilder::new(ui)
-            .size(Size::relative(0.5)) // top cell
-            .size(Size::remainder().at_most(30.))
-            .size(Size::remainder().at_least(300.)) // bottom cell
-            .vertical(|mut strip| {
-                // Add the top 'cell'
-                strip.cell(|ui| {
-                    
-                    MipsApp::register_table(&selected, ui);    
+        // Create right panel
+        egui::SidePanel::right("right_side_panel")
+            .resizable(false)
+            .show(ctx, |ui| {
+                // The right panel holding information about registers.
+                ui.horizontal(|ui| {
+                    egui::ComboBox::from_label("Data format")
+                        .selected_text(format!("{:?}", selected))
+                        .show_ui(ui, |ui| {
+                            // TODO: Add to so it actually impacts format of values.
+                            ui.selectable_value(selected, DataFormat::Hex, "Hex");
+                            ui.selectable_value(selected, DataFormat::Dec, "Decimal");
+                            ui.selectable_value(selected, DataFormat::Bin, "Binary");
+                        });
                 });
-                // Add cell for label table
-                strip.cell(|ui| {
-                    ui.heading("Label table");
-                });
-                // We add a nested strip in the bottom cell:
-                strip.cell(|ui| {
-                    MipsApp::symbol_table(&selected, ui);
-                });
-            });
 
-        });
+                // Create tables for registers and symbols
+                StripBuilder::new(ui)
+                    .size(Size::relative(0.5)) // top cell
+                    .size(Size::remainder().at_most(30.))
+                    .size(Size::remainder().at_least(300.)) // bottom cell
+                    .vertical(|mut strip| {
+                        // Add the top 'cell'
+                        strip.cell(|ui| {
+                            MipsApp::register_table(&selected, ui);
+                        });
+                        // Add cell for label table
+                        strip.cell(|ui| {
+                            ui.heading("Label table");
+                        });
+                        // We add a nested strip in the bottom cell:
+                        strip.cell(|ui| {
+                            MipsApp::symbol_table(&selected, ui);
+                        });
+                    });
+            });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.horizontal(|ui| {
+                ui.heading("Intsruction memory");
+                // Add buttons for stepping through the program.
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                    ui.heading("PC: 0");
+                    if ui.add(egui::Button::new("Step")).clicked() {
+                        // Reset memory
+                    }
+                    // Add Run button
+                    if ui.add(egui::Button::new("Run")).clicked() {
+                        // Run program
+                    }
+                    // Add step
+                    if ui.add(egui::Button::new("Reset")).clicked() {
+                        // Step forward in program
+                    }
+                });
+            });
+            ui.vertical(|ui| {
+                MipsApp::instruction_table(&selected, ui);
+            });
+            /*
+            StripBuilder::new(ui)
+                .size(Size::remainder().at_least(400.0)) // top cell
+                .size(Size::exact(50.0)) // bottom cell
+                .vertical(|mut strip| {
+                    // Add the top 'cell'
+                    strip.cell(|ui| {
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+                    });
+                });
+                */
         });
 
         if false {
@@ -287,9 +330,5 @@ impl eframe::App for MipsApp {
                 ui.label("You would normally chose either panels OR windows.");
             });
         }
-
-        
     }
-
-    
 }
--- mips_sim/src/main.rs
+++ mips_sim/src/main.rs
@@ -61,15 +61,37 @@ fn main() {
 */
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use egui::Vec2;
 use mips_sim::*;
-
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        always_on_top: false,
+        maximized: false,
+        decorated: true,
+        drag_and_drop_support: true,
+        icon_data: None,
+        initial_window_pos: None,
+        initial_window_size: Option::from(Vec2::new(1000 as f32, 800 as f32)),
+        min_window_size: Option::from(Vec2::new(600 as f32, 400 as f32)),
+        max_window_size: Option::from(Vec2::new(1000 as f32, 800 as f32)),
+        resizable: true,
+        transparent: true,
+        vsync: true,
+        multisampling: 0,
+        depth_buffer: 0,
+        stencil_buffer: 0,
+        fullscreen: false,
+        hardware_acceleration: eframe::HardwareAcceleration::Preferred,
+        renderer: eframe::Renderer::Glow,
+        follow_system_theme: false,
+        default_theme: eframe::Theme::Dark,
+        run_and_return: true,
+    };
     eframe::run_native(
         "eframe template",
         native_options,
